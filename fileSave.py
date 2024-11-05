a2505e3305345054711cf1ae483534f80c70f621
flobath <florian.gaudin@epitech.eu>
2024-11-05 16:58:14 +0100

    [ADD] save file with enter key

---
+++ fileSave.py
@@ -0,0 +1,21 @@
+import sys
+
+def save_board(lines):
+    line = ""
+    four_cells = [0, 0, 0, 0]
+    i = 0
+    print ("lines: ", lines)
+    while (i < lines.__len__()):
+        for (j) in range(4):
+            print("i: ", i)
+            if (i < lines.__len__()):
+                four_cells[j] = lines[i]
+            i += 1
+        line += chr(pack_four_cells(four_cells[0], four_cells[1], four_cells[2], four_cells[3]))
+        four_cells = [0, 0, 0, 0]
+    line += "\n"
+    with open("file.txt", "w") as file:
+        file.write(line)
+        
+def pack_four_cells(c1, c2, c3, c4):
+    return (c1 << 6) | (c2 << 4) | (c3 << 2) | c4
--- visualiser.py
+++ visualiser.py
@@ -1,93 +1,90 @@
 import arcade
 import sys
+from fileSave import save_board
 
 SCREEN_WIDTH = 1920
 SCREEN_HEIGHT = 1000
 
-def separate_four_cells(packed_byte):
-    c1 = (packed_byte >> 6) & 0b11
-    c2 = (packed_byte >> 4) & 0b11
-    c3 = (packed_byte >> 2) & 0b11
-    c4 = packed_byte & 0b11
+class MyGame(arcade.Window):
+    """ Main application class. """
+
+    def __init__(self, width, height):
+        super().__init__(width, height)
+        self.cells = []
+
+    def setup(self, cells):
+        self.cells = cells
+
+    def on_draw(self):
+        arcade.start_render()
+        self.draw_board()
+
+    def update(self, delta_time):
+        pass
     
-    return c1, c2, c3, c4
+    def on_key_release(self, symbol, modifiers):
+        if symbol == arcade.key.ESCAPE:
+            arcade.close_window()
+        if symbol == arcade.key.ENTER:
+            print("Cells: ", self.cells)
+            save_board(self.cells)
+
+    def draw_board(self):
+        radius = SCREEN_HEIGHT - 10
+        x = SCREEN_WIDTH / 2
+        y = SCREEN_HEIGHT / 2
+        arcade.set_background_color(arcade.color.WHITE)
+        arcade.draw_rectangle_filled(x, y, radius, radius, (205, 145, 90))
+        line_spacing = radius / (number_of_lines + 1)
+
+        for i in range(number_of_lines):
+            start_x = x - (radius / 2) + (i + 1) * line_spacing
+            start_y = y - (radius / 2) + line_spacing
+            end_x = start_x
+            end_y = y + (radius / 2) - line_spacing
+            arcade.draw_line(start_x, start_y, end_x, end_y, arcade.color.BLACK, 2)
+
+            start_y = y - (radius / 2) + (i + 1) * line_spacing
+            start_x = x - (radius / 2) + line_spacing
+            end_y = start_y
+            end_x = x + (radius / 2) - line_spacing
+            arcade.draw_line(start_x, start_y, end_x, end_y, arcade.color.BLACK, 2)
+
+        for pos in range(len(self.cells)):
+            color = arcade.color.WHITE if self.cells[pos] == 1 else arcade.color.BLACK
+            if self.cells[pos] in (1, 2):
+                cell_x = x - (radius / 2) + (pos % number_of_lines + 1) * line_spacing
+                cell_y = y - (radius / 2) + (pos // number_of_lines + 1) * line_spacing
+                arcade.draw_circle_filled(cell_x, cell_y, (line_spacing / 2), color)
+
+def separate_four_cells(packed_byte):
+    return ((packed_byte >> 6) & 0b11, (packed_byte >> 4) & 0b11, (packed_byte >> 2) & 0b11, packed_byte & 0b11)
 
 def analyse_content(file_content, number_of_lines):
-    pos_x = []
-    pos_y = []
-    coor_x = []
-    colors = []
     cells = []
+    for char in file_content:
+        cells.extend(separate_four_cells(ord(char)))
     
-    for (i) in range(file_content.__len__()):
-        cells = cells + list(separate_four_cells(ord(file_content[i])))
-    for pos in range(cells.__len__()):
-        if (cells[pos] == 1):
-            pos_x.append(pos % number_of_lines)
-            pos_y.append(pos // number_of_lines)
-            colors.append(arcade.color.WHITE)
-            coor_x.append(0)
-        if (cells[pos] == 2):
-            pos_x.append(pos % number_of_lines)
-            pos_y.append(pos // number_of_lines)
-            colors.append(arcade.color.BLACK)
-            coor_x.append(0)
-    draw(number_of_lines, pos_x, pos_y, coor_x, colors)
-    
-    
-
-def draw(number_of_lines, pos_x, pos_y, coor_x, colors):
-    radius = SCREEN_HEIGHT - 10
-    x = SCREEN_WIDTH / 2
-    y = SCREEN_HEIGHT / 2
-
-    arcade.open_window(SCREEN_WIDTH, SCREEN_HEIGHT, "Drawing Example")
-    arcade.set_background_color(arcade.color.WHITE)
-    arcade.start_render()
-    arcade.draw_rectangle_filled(x, y, radius, radius, (205, 145, 90))
-    line_spacing = radius / (number_of_lines + 1)
-
-    for i in range(number_of_lines):
-        start_x = x - (radius / 2) + (i + 1) * line_spacing
-        start_y = y - (radius / 2) + line_spacing
-        end_x = start_x
-        end_y = y + (radius / 2) - line_spacing
-        arcade.draw_line(start_x, start_y, end_x, end_y, arcade.color.BLACK, 2)
-
-    for i in range(number_of_lines):
-        start_y = y - (radius / 2) + (i + 1) * line_spacing
-        start_x = x - (radius / 2) + line_spacing
-        end_y = start_y
-        end_x = x + (radius / 2) - line_spacing
-        arcade.draw_line(start_x, start_y, end_x, end_y, arcade.color.BLACK, 2)
-    for i in range(number_of_lines):
-        if (i == pos_x):
-            coor_x = x - (radius / 2) + (i + 1) * line_spacing
-            break
-    for num in range(pos_x.__len__()):
-        for i in range(number_of_lines):
-            if (i == pos_x[num]):
-                coor_x[num] = x - (radius / 2) + (i + 1) * line_spacing
-                break
-        for i in range(number_of_lines):
-            if (i == pos_y[num]):
-                arcade.draw_circle_filled(coor_x[num], y - (radius / 2) + (i + 1) * line_spacing, (line_spacing / 2), colors[num])
-                break
-            
-    arcade.finish_render()
-    arcade.run()
+    return cells[:number_of_lines * number_of_lines]  # Limit to the board size
 
 def main():
+    global number_of_lines  # Keep it global for simplicity in drawing
     number_of_lines = 20
     file_name = ""
-    if (sys.argv.__len__() == 3):
-        number_of_lines = sys.argv[1]
+
+    if len(sys.argv) == 3:
+        number_of_lines = int(sys.argv[1])  # Convert to int
         file_name = sys.argv[2]
-    elif (sys.argv.__len__() == 2):
+    elif len(sys.argv) == 2:
         file_name = sys.argv[1]
-    file_content = open(file_name, "r")
-    analyse_content(file_content.read(), int(number_of_lines))
-    pass
+
+    with open(file_name, "r") as file:
+        file_content = file.read()
+        cells = analyse_content(file_content, number_of_lines)
+
+    game = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT)
+    game.setup(cells)
+    arcade.run()
 
 if __name__ == "__main__":
     main()
