c9e7a6f9622fc55daf614c2ba15f07d98648a904
flobath <florian.gaudin@epitech.eu>
2024-11-13 00:46:25 +0100

    [FIX] check if several messages are received at the same time

--- communication/src/ACommunication.cpp
+++ communication/src/ACommunication.cpp
@@ -9,14 +9,23 @@
 
 void ACommunication::analyse()
 {
-    std::string message;
+    std::string message = "";
+    std::string message2 = "";
     std::vector <std::string> tab;
     while (1) {
-        message = Receive();
+        if (message == "")
+            message = Receive();
+            if (message.find("\n") != std::string::npos) {
+                message2 = message.substr(message.find("\n") + 1);
+                message = message.substr(0, message.find("\n"));
+            } else {
+                message2 = "";
+            }
         while (message.find(" ") != std::string::npos) {
             tab.push_back(message.substr(0, message.find(" ")));
             message = message.substr(message.find(" ") + 1);
         }
+        message = message2;
         tab.push_back(message);
         if (tab[0] == "START") {
             start(tab);
